6144eaafbeb4648c6d3df8760b5549d98d713ddf
Chriszao <chris.f.assis18@gmail.com>
2022-06-25 20:35:18 -0300

    refactor: Add prefix categories, to path os categories route

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () =>
   console.log("✨ Server's running at http://localhost:3333"),
