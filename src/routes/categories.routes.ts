b97c6de2d62ff4fd6f5caac5cae8e8351b939b65
Chriszao <chris.f.assis18@gmail.com>
2022-06-25 20:36:34 -0300

    feat: Add list route and validate category already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,31 @@
 import { Request, Response, Router } from 'express';
 
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/categories', (request: Request, response: Response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists !== undefined) {
+    return response.status(400).json({
+      message: 'Category already exists!',
+    });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const categories = categoriesRepository.list();
+
+  return response.status(201).json(categories);
 });
 
 export { categoriesRoutes };
