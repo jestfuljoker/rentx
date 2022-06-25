06c7bff356826aefacb636797afe9e2fe229372c
Chriszao <chris.f.assis18@gmail.com>
2022-06-25 20:35:53 -0300

    feat: Add category repository and add create, list and findByName methods

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,34 @@
+import { Category } from '../model/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      createdAt: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category | undefined {
+    return this.categories.find(category => name === category.name);
+  }
+}
