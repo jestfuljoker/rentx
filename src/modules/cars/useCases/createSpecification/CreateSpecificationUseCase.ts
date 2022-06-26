12e0ae743005ff10959f27bf077f54593b2e4b3c
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 15:07:59 -0300

    feat: Add category and specification use case and controller

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,21 @@
+import { SpecificationsRepository } from '../../repositories/implementations/SpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateSpecificationUseCase {
+  constructor(private specificationRepository: SpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists!`);
+    }
+
+    this.specificationRepository.create({ name, description });
+  }
+}
