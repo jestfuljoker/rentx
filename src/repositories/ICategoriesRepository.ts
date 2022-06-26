9cd20e00e243eeb91fb45bbc7db2552261233788
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 11:36:34 -0300

    feat: Add ICategoriesRepository interface

---
+++ src/repositories/ICategoriesRepository.ts
@@ -0,0 +1,12 @@
+import { Category } from '../model/Category';
+
+export interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export interface ICategoriesRepository {
+  findByName(name: string): Category;
+  list(): Category[];
+  create({ name, description }: ICreateCategoryDTO): void;
+}
