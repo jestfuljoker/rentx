8015f0daf722c7ce7dd6a7fe5c6da6678756b975
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 13:57:20 -0300

    refactor: Move business rule to controller file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,16 @@
 import { Request, Response, Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
-
-categoriesRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({
-    description,
-    name,
-  });
-
-  return response.status(201).send();
+categoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request: Request, response: Response) => {
-  const categories = categoriesRepository.list();
-
-  return response.status(201).json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
