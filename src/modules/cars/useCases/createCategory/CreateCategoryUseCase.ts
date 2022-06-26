3ca23ddaafac29effb97f87e3f0cfc204abe6767
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 13:54:27 -0300

    refactor: Convert services to useCases

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,18 +1,18 @@
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-export class CreateCategoryService {
+export class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists !== undefined) {
-      throw new Error('Category already exists!');
+      throw new Error(`Category ${name} already exists!`);
     }
 
     this.categoriesRepository.create({ name, description });
