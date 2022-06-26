94eaebc50f15989d6bf0ac5c39ce992cffe0d456
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 15:07:30 -0300

    refactor: Add singleton instance export to SpecificationsRepository

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -7,8 +7,18 @@ import {
 export class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
-  constructor() {
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
     this.specifications = [];
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+
+    return SpecificationsRepository.INSTANCE;
   }
 
   list(): Specification[] {
