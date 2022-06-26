7207c8305d670a9124b52f44812966c6434f4ee1
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 13:53:06 -0300

    feat: Add specifications routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 app.listen(3333, () =>
   console.log("✨ Server's running at http://localhost:3333"),
