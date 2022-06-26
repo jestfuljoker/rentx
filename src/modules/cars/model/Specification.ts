4084a0ff6ee16d4254e1ddf477d768e7965b04aa
Chriszao <chris.f.assis18@gmail.com>
2022-06-26 13:53:48 -0300

    feat: Add Specification entitie

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,14 @@
+import { v4 as uuidV4 } from 'uuid';
+
+export class Specification {
+  id?: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
