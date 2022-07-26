4f5b16102d4f15abcd9edda622bf9dff1f66652b
Chrizao <chris.f.assis18@gmail.com>
2022-07-26 13:26:58 -0300

    refactor: Change model folder name to entities. Add decorators from typeorm to Category entitiy

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('categories')
 export class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   createdAt: Date;
 
   constructor() {
--- src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../../model/Specification';
+import { Specification } from '../../entities/Specification';
 import { SpecificationsRepository } from '../../repositories/implementations/SpecificationsRepository';
 
 export class ListSpecificationsUseCase {
