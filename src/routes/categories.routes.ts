3fd4e08b9d8c3f9924db13d6e6968fb90ec983d0
Chrizao <chris.f.assis18@gmail.com>
2022-11-08 19:44:52 -0300

    refactor: Refactored controller handling

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { importCategoriesController } from '../modules/cars/useCases/importCategories';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import createCategoryController from '../modules/cars/useCases/createCategory';
+import importCategoriesController from '../modules/cars/useCases/importCategories';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const upload = multer({
   dest: path.resolve(__dirname, '../', '../', 'tmp'),
@@ -13,15 +13,15 @@ const upload = multer({
 const categoriesRoutes = Router();
 
 categoriesRoutes.post('/', (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoriesController.handle(request, response);
+  return importCategoriesController().handle(request, response);
 });
 
 export { categoriesRoutes };
