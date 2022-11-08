e0e01fb6c796c2889c090e4ed347bd4e41fd80e4
Chrizao <chris.f.assis18@gmail.com>
2022-11-08 19:42:50 -0300

    refactor: Change database import position

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
+import './database';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
 import swaggerFile from './swagger.json';
-
-import './database';
 
 const app = express();
 
