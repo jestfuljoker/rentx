218030dcd37c0ea1c83c78a92589258a1f8f3adc
Chrizao <chris.f.assis18@gmail.com>
2022-11-08 22:34:56 -0300

    feat: Add User entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driverLicense: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
