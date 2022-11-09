5207e56127c7d17042270d3e3900d05049ac0c8e
Chrizao <chris.f.assis18@gmail.com>
2022-11-08 22:35:42 -0300

    feat: Add User controller and use case to create an user

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, driverLicense } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      driverLicense,
+    });
+
+    return response.status(201).send();
+  }
+}
---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,33 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUserRepository';
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    password,
+    email,
+    ...userData
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      ...userData,
+      email,
+      password: passwordHash,
+    });
+  }
+}
