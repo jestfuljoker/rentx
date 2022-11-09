7365627469e2fbeb80df06c4ca2623a451b240d7
Chrizao <chris.f.assis18@gmail.com>
2022-11-08 21:27:56 -0300

    feat: Add typeorm decorator in specification entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,12 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('specifications')
 export class Specification {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @CreateDateColumn()
   createdAt: Date;
 
   constructor() {
