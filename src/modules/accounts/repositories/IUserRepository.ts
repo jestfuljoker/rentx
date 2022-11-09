8682b2a7a0edf8a4c15d30b8a52e9219b5bc5d93
Chrizao <chris.f.assis18@gmail.com>
2022-11-09 20:41:35 -0300

    feat: Add findById method to users repository

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -4,4 +4,5 @@ import { User } from '../entities/User';
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
 }
