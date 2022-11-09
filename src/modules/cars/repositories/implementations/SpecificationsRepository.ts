67d8ff9db2d91b03fe287c5102838a962c94d8af
Chrizao <chris.f.assis18@gmail.com>
2022-11-09 20:42:18 -0300

    refactor: Fix return type of findByName method

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -19,7 +19,7 @@ export class SpecificationsRepository implements ISpecificationsRepository {
     return specifications;
   }
 
-  async findByName(name: string): Promise<Specification> {
+  async findByName(name: string): Promise<Specification | undefined> {
     const specification = await this.repository.findOne({ name });
 
     return specification;
