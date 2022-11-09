4c0943c59f6333c310f21862c8d29bd39ff277f7
Chrizao <chris.f.assis18@gmail.com>
2022-11-09 07:52:28 -0300

    feat: Add user authentication with jwt use case

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUserRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    console.log(process.env);
+
+    const token = jwt.sign({}, process.env.JWT_SECRET, {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      token,
+      user: {
+        name: user.name,
+        email,
+      },
+    };
+  }
+}
