50b6b43c9d5980c9a2e94e525f5c68064f354f5f
Chrizao <chris.f.assis18@gmail.com>
2022-11-10 11:44:57 -0300

    feat: Add config to upload files

---
+++ src/config/upload.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import { resolve } from 'path';
+
+interface IUploadConfig {
+  storage: multer.StorageEngine;
+}
+
+export default {
+  upload(folder: string): IUploadConfig {
+    return {
+      storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', folder),
+        filename: (_, file, callback) => {
+          const fileHash = crypto.randomBytes(16).toString('hex');
+
+          const fileName = `${fileHash}-${file.originalname}`;
+
+          return callback(null, fileName);
+        },
+      }),
+    };
+  },
+};
