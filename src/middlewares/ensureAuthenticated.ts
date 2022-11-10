ec5f084d06a00d156edc3970cd5eb98f594c52fc
Chrizao <chris.f.assis18@gmail.com>
2022-11-10 11:45:49 -0300

    feat: Add middleware to ensure if user is authenticated

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UserRepository';
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('User not authorized. Missing token', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: userId } = verify(token, process.env.JWT_SECRET as string);
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(userId as string);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+    request.user = {
+      id: userId as string,
+    };
+
+    next();
+  } catch (error) {
+    throw new AppError('User not authorized. Invalid token', 401);
+  }
+}
