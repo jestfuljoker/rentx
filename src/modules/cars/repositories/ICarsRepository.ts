f6b246c41f250c500943eb462afa8aa3f495f776
Chrizao <chris.f.assis18@gmail.com>
2022-11-24 11:01:42 -0300

    feat: Add findAvailable cars repository method

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,4 +4,9 @@ import { Car } from '../infra/typeorm/entities/Car';
 export interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(licensePlate: string): Promise<Car | undefined>;
+  findAvailable(
+    brand?: string,
+    categoryId?: string,
+    name?: string,
+  ): Promise<Car[]>;
 }
