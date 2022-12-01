793a78403a6bbc751be5237c113d7957ed4abedd
Chrizao <chris.f.assis18@gmail.com>
2022-12-01 07:58:54 -0300

    feat: Add findById method

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -9,4 +9,5 @@ export interface ICarsRepository {
     categoryId?: string,
     name?: string,
   ): Promise<Car[]>;
+  findById(id: string): Promise<Car | undefined>;
 }
--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -9,4 +9,5 @@ export interface ISpecificationsRepository {
   create({ name, description }: ICreateSpecificationDTO): Promise<void>;
   findByName(name: string): Promise<Specification | undefined>;
   list(): Promise<Specification[]>;
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
